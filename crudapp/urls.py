adbecd5e9150ab72a6fbb19bd88515209a152e64
divinejoshua <divineuche399@gmail.com>
2022-05-05 11:05:29 +0100

    updated the list page

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+
+# Url namespace 
+app_name = 'crudapp'
+
 urlpatterns = [
     path('', views.AddHomeView.as_view(), name='addItem'),
     path('list/', views.ListItems.as_view(), name='listItem'),
